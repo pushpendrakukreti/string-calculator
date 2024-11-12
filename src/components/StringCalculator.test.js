9e52dd6419f35158bc7606421d9d0bf4efec1d9c
pkukreti <pkukreti@egnyte.com>
2024-11-12 21:20:31 +0530

    chore: implementation for single input string

--- src/components/StringCalculator.test.js
+++ src/components/StringCalculator.test.js
@@ -1,5 +1,9 @@
-import { add } from './StringCalculator';
+import { add } from "./StringCalculator";
 
-test('returns 0 for an empty string', () => {
+test("returns 0 for an empty string", () => {
   expect(add("")).toBe(0);
+});
+
+test("returns the number itself if one number is provided", () => {
+  expect(add("1")).toBe(1);
 });
