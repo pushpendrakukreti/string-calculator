eb1c979f99af742fdc97d3fb5183cc1d3672cf0d
pkukreti <pkukreti@egnyte.com>
2024-11-12 21:31:48 +0530

    chore: implementation for handling negative numbers

--- src/components/StringCalculator.test.js
+++ src/components/StringCalculator.test.js
@@ -19,3 +19,7 @@ test("handles new lines as delimiters", () => {
 test("supports custom delimiters", () => {
   expect(add("//;\n1;2")).toBe(3);
 });
+
+test("throws an error for negative numbers", () => {
+  expect(() => add("1,-2,-3")).toThrow("negative numbers not allowed: -2, -3");
+});
